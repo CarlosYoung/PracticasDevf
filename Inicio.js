a37038f257f6c0930b016a6cb7d3da7e1060e1a8
carlos <carlosflat01@gmail.com>
2018-04-20 20:49:06 -0500

    nuevo codigo

---
+++ Inicio.js
@@ -0,0 +1,27 @@
+
+ let edad=20;
+ if (edad<18) {
+     
+    if (edad>=15 && edad <=18 ){
+        console.log("estas en la prepa")
+    }
+    if (edad>=6 && edad <=12){
+        console.log("estas en la primaria")
+    }
+    if (edad>12 && edad <=14){
+        console.log("estas en la secuendaria")
+    }
+    if (edad>=3 && edad<=6){
+        console.log("estas en el kinder")
+    }
+    if (edad<3){
+        console.log('estas beibi');
+    }
+}
+else{
+         console.log("universidad");
+     }
+     
+     
+    
+    
