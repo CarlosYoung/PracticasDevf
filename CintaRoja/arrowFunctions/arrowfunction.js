84e515e8a1d3da822b5a2fcdad06264093a5d9af
carlos <carlosflat01@gmail.com>
2018-06-05 21:47:49 -0500

    archivos nuevos

---
+++ CintaRoja/arrowFunctions/arrowfunction.js
@@ -0,0 +1,13 @@
+/*const suma=(a,b)=>{
+    return a+b;
+}
+let resultado=suma(2,9);
+console.log(resultado);
+
+const input=prompt("escribe algo");
+const getInput=text=>`input:${text}`;
+console.log(getInput(input));*/
+
+const input=prompt("Cantidad en pesos");
+const converter=value=>value*20.67;
+console.log(converter(number(input)))
---
+++ CintaRoja/arrowFunctions/numeromasgrande.js
@@ -0,0 +1,29 @@
+/*var numero=[5,5,2,7,6,7,8]
+numero.sort();
+console.log(numero);
+for(let i=0;i<numero.length;i++){
+    if (i==numero.length-1){
+        console.log(numero[i])
+    }
+}*/
+
+/*var numero=[1,2,3,4,6,7,8,9,10]
+function Suma(numero,numero2){
+    return numero+numero2;
+}
+console.log(numero.reduce(Suma));*/
+
+/*var numero=[1,2,3,4,5,6,8,9,10]
+function Suma(numero,numero2){
+    return numero+numero2;
+}
+console.log(55-numero.reduce(Suma));*/
+
+var numero=[1,2,5,8,7]
+for(let i=0;i<numero.length;i++){
+    resultado=numero%i;
+    if(resultado==0){
+    console.log(i)
+    }
+}
+
